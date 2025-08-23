70b8f73c74380d0745c4e2e0ab64da130f20f067
Ivy Zhao <ivyivyzhao77@gmail.com>
2025-08-23 00:08:22 -0400

    minor update and add extract_motifs (template?)

--- method/run.sh
+++ method/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python motif_library/context_graph_builder.py --dataset data/30ppl_eval_v1_0728 --out intermediate_data/30ppl_eval_v1_0728 --participant 1962Michael --llm_cfg context_graph/qwen_context_default.yaml
+python motif_library/context_graph_builder.py --dataset data/30ppl_eval_v1_0728 --out intermediate_data/30ppl_eval_v1_0728/context_graphs --participant 1962Michael --llm_cfg context_graph/qwen_context_default.yaml
+
+python motif_library/extract_motifs.py --participant_dir intermediate_data/30ppl_eval_v1_0728/context_graphs/1962Michael --output_dir intermediate_data/30ppl_eval_v1_0728/motif_library
